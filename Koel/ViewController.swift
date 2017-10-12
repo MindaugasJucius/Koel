72b58e8b6b55bca2c86858d1c40ee83d7ed2096f
MindaugasJucius <MindaugasJucius@icloud.com>
2017-10-12 19:40:40 +0300

    Initial Commit

---
+++ Koel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Koel
+//
+//  Created by Mindaugas Jucius on 10/12/17.
+//  Copyright © 2017 Mindaugas Jucius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
