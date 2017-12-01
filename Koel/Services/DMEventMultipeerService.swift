6be8409d960bf58a0b6eda0e6a0e81d153c58dad
Mindaugas Jucius <mindaugas.jucius@icloud.com>
2017-12-01 12:39:01 +0200

    finish wrapping multipeer connection

--- Koel/Services/DMEventMultipeerService.swift
+++ Koel/Services/DMEventMultipeerService.swift
@@ -47,10 +47,68 @@ class DMEventMultipeerService: NSObject {
     
         advertiser.delegate = self
         browser.delegate = self
+        session.delegate = self
     }
     
-    func startAdvertisingEvent() {
+    //MARK: - Observables
+    
+    func incomingPeerConnections() -> Observable<(MCPeerID, [String: Any]?, (Bool) -> ())> {
+        return incomingConnections
+            .map { [unowned self] (client, context, handler) in
+                // Do not expose session to observers
+                return (client, context, { (accept: Bool) in
+                        handler(accept, self.session)
+                })
+        }
+    }
+
+    func peerConnectionErrors() -> Observable<Error> {
+        return connectionErrors
+    }
+    
+    func connectedPeers() -> Observable<[MCPeerID]> {
+        return connections.asObservable()
+    }
+    
+    func nearbyFoundPeers() -> Observable<[(MCPeerID, [String: String]?)]> {
+        return nearbyPeers.asObservable()
+    }
+    
+    //MARK: - Lifecycle management
+    
+    func disconnect() {
+        self.session.disconnect()
+    }
+    
+    //MARK: - Advertising
+    
+    func startAdvertising() {
         advertiser.startAdvertisingPeer()
+    }
+    
+    func stopAdvertising() {
+        advertiser.stopAdvertisingPeer()
+    }
+
+    //MARK: - Browsing
+    
+    func startBrowsing() {
+        browser.startBrowsingForPeers()
+    }
+    
+    func stopBrowsing() {
+        browser.stopBrowsingForPeers()
+        nearbyPeers.value = []
+    }
+    
+    func connect(_ peer: MCPeerID, context: [String: Any]?, timeout: TimeInterval) {
+        guard let context = context,
+            let data = try? JSONSerialization.data(withJSONObject: context, options: JSONSerialization.WritingOptions()) else {
+            browser.invitePeer(peer, to: self.session, withContext: nil, timeout: timeout)
+            return
+        }
+    
+        browser.invitePeer(peer, to: self.session, withContext: data, timeout: timeout)
     }
     
     /// Retrieve MCPeerID from UserDefaults if one exists, or create and store a new one
@@ -77,15 +135,26 @@ class DMEventMultipeerService: NSObject {
 extension DMEventMultipeerService: MCNearbyServiceBrowserDelegate {
     
     func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
-        browser.invitePeer(peerID, to: session, withContext: nil, timeout: 10)
+        var result: [(MCPeerID, [String: String]?)] = []
+        for peerIDWithInfo in (self.nearbyPeers.value + [(peerID, info)]) {
+            
+            if (result.map { $0.0 }).index(of: peerIDWithInfo.0) == nil {
+                result = result + [peerIDWithInfo]
+            }
+            
+        }
+        
+        self.nearbyPeers.value = result
     }
     
     func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
-        
+        nearbyPeers.value = nearbyPeers.value.filter { (existingPeerID, _) -> Bool in
+            return existingPeerID != peerID
+        }
     }
     
     func browser(_ browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: Error) {
-        
+        connectionErrors.onNext(error)
     }
     
 }
@@ -93,11 +162,45 @@ extension DMEventMultipeerService: MCNearbyServiceBrowserDelegate {
 extension DMEventMultipeerService: MCNearbyServiceAdvertiserDelegate {
     
     func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: Error) {
-        print("did not start advertising due to: \(error)")
+        connectionErrors.onNext(error)
     }
     
     func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
-        print("received invitation from peer: \(peerID)")
+        
+        guard let context = context, let json = try? JSONSerialization.jsonObject(with: context) else {
+            incomingConnections.onNext((peerID, nil, invitationHandler))
+            return
+        }
+
+        incomingConnections.onNext((peerID, json as? [String: Any], invitationHandler))
     }
     
 }
+
+extension DMEventMultipeerService: MCSessionDelegate {
+
+    public func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        // Only emit to observers when peer has connected
+        if state != .connecting {
+            connections.value = session.connectedPeers
+        }
+    }
+    
+    public func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    public func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    public func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    public func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        
+    }
+    
+}
+
