52ba55d675b126e3bbbb5cf105bf43e613411c39
Mindaugas Jucius <mindaugas.jucius@icloud.com>
2018-03-12 10:27:36 +0200

    add SongSharingViewModelType protocol

---
+++ Koel/Protocols/SongSharingViewModelType.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  Koel
+//
+//  Created by Mindaugas on 12/03/2018.
+//  Copyright © 2018 Mindaugas Jucius. All rights reserved.
+//
+
+import Foundation
+
+protocol SongSharingViewModelType {
+    var songSharingViewModel: DMEventSongSharingViewModelType { get }
+}
