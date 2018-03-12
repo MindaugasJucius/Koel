f0f19d60db81ca4f7a6f4c9114f1e13f5c8e1af5
Mindaugas Jucius <mindaugas.jucius@icloud.com>
2018-03-12 10:04:49 +0200

    add ReusableView protocol
    contains reuseIdentifier property

---
+++ Koel/Protocols/ReusableView.swift
@@ -0,0 +1,23 @@
+//
+//  ReusableViewType.swift
+//  Koel
+//
+//  Created by Mindaugas on 12/03/2018.
+//  Copyright © 2018 Mindaugas Jucius. All rights reserved.
+//
+
+import UIKit
+
+protocol ReusableView where Self: UIView {
+    
+    static var reuseIdentifier: String { get }
+    
+}
+
+extension ReusableView {
+    
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+    
+}
