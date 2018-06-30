a4a4bb8d02fbe470d528c31f50e4a34a2a912c6f
Mindaugas Jucius <mindaugas.jucius@icloud.com>
2018-06-30 19:05:54 +0300

    Use subtitle UITableViewCellStyle for Spotify Song Cell

--- Koel/Song Search/DMSpotifySongTableViewCell.swift
+++ Koel/Song Search/DMSpotifySongTableViewCell.swift
@@ -9,42 +9,10 @@
 import UIKit
 
 class DMSpotifySongTableViewCell: UITableViewCell, ReusableView {
-    
-    private lazy var titleLabel: UILabel = {
-        let label = UILabel()
-        label.apply(SongCellStylesheet.titleLabelStyle)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    private lazy var artistTitleLabel: UILabel = {
-        let label = UILabel()
-        label.apply(SongCellStylesheet.subtitleLabelStyle)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    private lazy var stackView: UIStackView = {
-        let labelStackView = UIStackView()
-        labelStackView.translatesAutoresizingMaskIntoConstraints = false
-        labelStackView.axis = .vertical
-        labelStackView.addArrangedSubview(titleLabel)
-        labelStackView.addArrangedSubview(artistTitleLabel)
-        labelStackView.spacing = 3
-        return labelStackView
-    }()
-    
+
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        addSubview(stackView)
-        
-        let constraints = [stackView.leftAnchor.constraintEqualToSystemSpacingAfter(safeAreaLayoutGuide.leftAnchor, multiplier: 2),
-                           safeAreaLayoutGuide.rightAnchor.constraintEqualToSystemSpacingAfter(stackView.rightAnchor, multiplier: 1),
-                           stackView.topAnchor.constraintEqualToSystemSpacingBelow(safeAreaLayoutGuide.topAnchor, multiplier: 1),
-                           safeAreaLayoutGuide.bottomAnchor.constraintEqualToSystemSpacingBelow(stackView.bottomAnchor, multiplier: 1)
-                          ]
-        
-        NSLayoutConstraint.activate(constraints)
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        imageView?.backgroundColor = .blue
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -52,7 +20,7 @@ class DMSpotifySongTableViewCell: UITableViewCell, ReusableView {
     }
     
     func configure(withSong song: DMSearchResultSong) {
-        titleLabel.text = song.title
-        artistTitleLabel.text = song.artistName
+        textLabel?.text = song.title
+        detailTextLabel?.text = song.artistName
     }
 }
